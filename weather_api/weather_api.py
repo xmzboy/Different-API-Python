39baa477f12eb07cbfe8581e082bc8e469c0e12a
Ivan Andreichuk <vanichpz@gmail.com>
2022-08-31 16:02:19 +0300

    Move files in folders

--- weather_api/weather_api.py
+++ weather_api/weather_api.py
@@ -4,11 +4,14 @@ from tkinter import *
 from tkinter.ttk import Combobox
 from random import uniform
 import emoji
-
-places = [('59,9386', '30,3141'), ('61,7632', '31,0061'), ('-45.0311', '168.6626'), ('50.0880', '14.4207'), ('54.7064', '20.5109')]
+import time
+start = time.time()
+places = [('59,94', '30,314'), ('61,763', '31,006'), ('-45.031', '168.6626'), ('50.09', '14.42'), ('54.7064', '20.511')]
 good_place = []
 
-weather_status = {'Rain': emoji.emojize('rain :cloud_with_rain:'), 'Clear': emoji.emojize('sun :sun:'), 'Clouds': emoji.emojize('clouds :cloud:')}
+weather_status = {'Rain': emoji.emojize('rain :umbrella_with_rain_drops:'), 'Clear': emoji.emojize('sun :sun:'),
+                  'Clouds': emoji.emojize('clouds :cloud:'), 'Snow': emoji.emojize('snow :snowflake:'),
+                  'Fog': emoji.emojize('fog :fog:')}
 
 for i in range(5):
     places.append((uniform(-90, 90), uniform(-180, 180)))
@@ -33,12 +36,13 @@ def show_result(dct):
             weather = weather_status[dct['weather'][0]['main']]
         except:
             weather = dct['weather'][0]['main']
-        txt.insert(INSERT, f"Location: {dct['name']}, {dct['sys']['country']}\n"
-                           f"Weather: {weather}\n"
-                           f"Temperature: {round(dct['main']['temp'] - 273)}\n"
-                           f"Feels like: {round(dct['main']['feels_like'] - 273)}\n"
-                           f"Humidity: {round(dct['main']['humidity'])}\n"
-                           f"Wind speed: {dct['wind']['speed']}\n")
+        txt.insert(INSERT, f"{emoji.emojize(':globe_showing_Americas:')}Location: {dct['name']}, {dct['sys']['country']}\n"
+                           f"{emoji.emojize(':seedling:')}Weather: {weather}\n"
+                           f"{emoji.emojize(':fire:')}Temperature: {round(dct['main']['temp'] - 273)}\n"
+                           f"{emoji.emojize(':fallen_leaf:')}Feels like: {round(dct['main']['feels_like'] - 273)}\n"
+                           f"{emoji.emojize(':droplet:')}Humidity: {round(dct['main']['humidity'])}\n"
+                           f"{emoji.emojize(':cyclone:')}Wind speed: {dct['wind']['speed']}\n"
+                           f"{emoji.emojize(':globe_with_meridians:')}Coordinates: {dct['coord']['lat']}, {dct['coord']['lon']}")
     except:
         txt.delete(1.0, END)
         txt.insert(INSERT, f"Information is absent")
@@ -51,6 +55,7 @@ def set_coord():
     good_place.append((str(lats_entry.get()), str(lons_entry.get())))
     dct = connection(good_place, -1)
     show_result(dct)
+
 
 window = Tk()
 window.title("Weather Here")
@@ -82,4 +87,8 @@ set_but.pack()
 txt = Text(window, width=30, height=10, font=("Arial Bold", 16))
 txt.pack(side=LEFT, expand=True)
 
+dct = connection(good_place, int(combo['values'].index('Novaya Gollandiya')))
+show_result(dct)
+
+print(time.time() - start)
 window.mainloop()
