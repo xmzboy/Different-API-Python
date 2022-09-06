3b15949b52054fb3b456ea1e06716b91d2171212
Ivan Andreichuk <vanichpz@gmail.com>
2022-09-06 16:42:03 +0300

    Create constants.py

---
+++ weather_api/constants.py
@@ -0,0 +1,8 @@
+import emoji
+
+
+WEATHER_STATUS = {'Rain': emoji.emojize('rain :umbrella_with_rain_drops:'),
+                  'Clear': emoji.emojize('sun :sun:'),
+                  'Clouds': emoji.emojize('clouds :cloud:'),
+                  'Snow': emoji.emojize('snow :snowflake:'),
+                  'Fog': emoji.emojize('fog :fog:')}
