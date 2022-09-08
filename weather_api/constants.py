131d8a739521926faaa611217845f1eee6185043
Ivan Andreichuk <vanichpz@gmail.com>
2022-09-08 10:51:54 +0300

    Delete async weather api from this project

--- weather_api/constants.py
+++
@@ -1,8 +0,0 @@
-import emoji
-
-
-WEATHER_STATUS = {'Rain': emoji.emojize('rain :umbrella_with_rain_drops:'),
-                  'Clear': emoji.emojize('sun :sun:'),
-                  'Clouds': emoji.emojize('clouds :cloud:'),
-                  'Snow': emoji.emojize('snow :snowflake:'),
-                  'Fog': emoji.emojize('fog :fog:')}
